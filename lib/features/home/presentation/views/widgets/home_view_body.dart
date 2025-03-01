dae01af4475d2ae327d91da2bc0269bec661bafb
ahmedkhaled130 <ak782060@gmail.com>
2025-03-01 10:44:00 +0200

    best seller list view done

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,9 +1,8 @@
 import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/styles.dart';
-import 'best_seller_list.dart';
+import 'best_seller_list_view.dart';
 import 'custom_app_bar.dart';
 import 'featured_list_view.dart';
 
@@ -12,29 +11,39 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          FeaturedBooksListView(),
-          SizedBox(
-            height: 50,
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: CustomAppBar(),
+            ),
+            const FeaturedBooksListView(),
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Text(
+                "Best Seller",
+                style: Styles.textStyle18?.copyWith(fontFamily: kGtSectraFine),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ]),
+        ),
+        const SliverToBoxAdapter(
+          child: Padding(
+            padding:  EdgeInsets.symmetric(horizontal: 30),
+            child: BestSellerListView(),
           ),
-          Text(
-            "Best Seller",
-            style: Styles.textStyle18?.copyWith(fontFamily: kGtSectraFine),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          BestSellerListViewItem(),
-
-        ],
-      ),
+        )
+      ],
     );
   }
 }
-
 
