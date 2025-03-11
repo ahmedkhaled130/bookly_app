6f8c85fb23a22f8c7b4eb1ad92afa19481f937b5
ahmedkhaled130 <ak782060@gmail.com>
2025-03-11 14:51:31 +0200

    refactor book details view

---
+++ lib/features/home/presentation/views/widgets/book_details_section.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/styles.dart';
+import 'book_rating.dart';
+import 'custom_book_details_app_bar.dart';
+import 'custom_list_view_item.dart';
+
+class BookDetailsSection extends StatelessWidget {
+  const BookDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+
+    return Column(
+      children: [
+        const CustomBookDetailsAppBar(),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * .22),
+          child: const CustomBookImage(),
+        ),
+        const SizedBox(
+          height: 43,
+        ),
+        const Text(
+          "A Little Life",
+          style: Styles.textStyle30,
+        ),
+        const SizedBox(
+          height: 3,
+        ),
+        Opacity(
+          opacity: .70,
+          child: Text(
+            "Hanya Yanagih",
+            style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+                fontFamily: "kGtSectraFine"),
+          ),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BookRating(),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,18 +1,15 @@
 import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:flutter/material.dart';
 
+import 'book_details_section.dart';
 import 'books_action.dart';
-import 'custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: CustomScrollView(
@@ -20,68 +17,39 @@ class BookDetailsViewBody extends StatelessWidget {
           SliverFillRemaining(
             hasScrollBody: false,
             child: Column(
-            children: [
-              const CustomBookDetailsAppBar(),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: width * .22),
-                child: const CustomBookImage(),
-              ),
-              const SizedBox(
-                height: 43,
-              ),
-              const Text(
-                "A Little Life",
-                style: Styles.textStyle30,
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              Opacity(
-                opacity: .70,
-                child: Text(
-                  "Hanya Yanagih",
-                  style: Styles.textStyle18.copyWith(
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w500,
-                      fontFamily: "kGtSectraFine"),
+              children: [
+                 BookDetailsSection(),
+                const SizedBox(
+                  height: 37,
                 ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  BookRating(),
-                ],
-              ),
-              const SizedBox(
-                height: 37,
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 12),
-                child: BooksAction(),
-              ),
-              const Expanded(
-                child: SizedBox(
-                  height: 50,
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12),
+                  child: BooksAction(),
                 ),
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "You can also like",
-                  style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+                const Expanded(
+                  child: SizedBox(
+                    height: 50,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 18,),
-              const SimilarBooksListView(),
-              const SizedBox(height: 26,),
-
-            ],
-          ),)
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "You can also like",
+                    style: Styles.textStyle14
+                        .copyWith(fontWeight: FontWeight.w600),
+                  ),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                const SimilarBooksListView(),
+                const SizedBox(
+                  height: 26,
+                ),
+              ],
+            ),
+          )
         ],
-
       ),
     );
   }
