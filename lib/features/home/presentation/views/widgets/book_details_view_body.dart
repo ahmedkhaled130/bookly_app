eacc784910e2809c98f106d41454cee133e4e304
ahmedkhaled130 <ak782060@gmail.com>
2025-03-11 14:42:51 +0200

    add custom scroll view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -15,64 +15,73 @@ class BookDetailsViewBody extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        children: [
-          const CustomBookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .22),
-            child: const CustomBookImage(),
-          ),
-          const SizedBox(
-            height: 43,
-          ),
-          const Text(
-            "A Little Life",
-            style: Styles.textStyle30,
-          ),
-          const SizedBox(
-            height: 3,
-          ),
-          Opacity(
-            opacity: .70,
-            child: Text(
-              "Hanya Yanagih",
-              style: Styles.textStyle18.copyWith(
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w500,
-                  fontFamily: "kGtSectraFine"),
-            ),
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      child: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
             children: [
-              BookRating(),
-            ],
-          ),
-          const SizedBox(
-            height: 37,
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 12),
-            child: BooksAction(),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "You can also like",
-              style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
-            ),
-          ),
-          const SizedBox(height: 18,),
-          const SimilarBooksListView(),
-          const SizedBox(height: 26,),
+              const CustomBookDetailsAppBar(),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * .22),
+                child: const CustomBookImage(),
+              ),
+              const SizedBox(
+                height: 43,
+              ),
+              const Text(
+                "A Little Life",
+                style: Styles.textStyle30,
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              Opacity(
+                opacity: .70,
+                child: Text(
+                  "Hanya Yanagih",
+                  style: Styles.textStyle18.copyWith(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: "kGtSectraFine"),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  BookRating(),
+                ],
+              ),
+              const SizedBox(
+                height: 37,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12),
+                child: BooksAction(),
+              ),
+              const Expanded(
+                child: SizedBox(
+                  height: 50,
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "You can also like",
+                  style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+                ),
+              ),
+              const SizedBox(height: 18,),
+              const SimilarBooksListView(),
+              const SizedBox(height: 26,),
 
+            ],
+          ),)
         ],
+
       ),
     );
   }
